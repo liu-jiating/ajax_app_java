ba27d76bdde6393d60338d63912dcc184403326a
liu-jiating <liu.jiating@smart-group.co.jp>
2025-05-13 15:45:53 +0900

    JSでレスポンスを処理する

--- src/main/resources/static/js/memo.js
+++ src/main/resources/static/js/memo.js
@@ -1,3 +1,18 @@
+const buildHTML = (XHR) => {
+  const item  = XHR.response;
+      const html = 
+      `
+        <div class="post">
+          <div class="post-date">
+            投稿日時：${item.createdAt}
+          </div>
+          <div class="post-content">
+            ${item.content}
+          </div>
+        </div>`;
+    return html;
+}
+
 function post() {
   // console.log("イベント発火")
 
@@ -8,12 +23,24 @@ function post() {
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    
+
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
-
-  })
+    XHR.onload = () => {
+      // console.log(XHR.response);
+      if (XHR.status != 200){
+        alert(`Error ${XHR.status}: ${XHR.response.error}`);
+        return null;
+      }
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      // console.log(formText.value);
+      
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
+  });
 };
 
 window.addEventListener("load", post);
